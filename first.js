284c421089f1f50ec1d1fd9a2b82553159cdf570
Shivam Yadav <shivamy13072002@gmail.com>
2026-08-20 12:48:13 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,64 @@
+ let userScore=0;
+ let computerScore=0;
+
+ const choices=document.querySelectorAll(".choice");
+ const msg=document.querySelector("#msg");
+
+ const userScorePara=document.querySelector("#user-score");
+ const computerScorePara=document.querySelector("#computer-score");
+
+ const genComputerChoice=()=>{
+    const options=["rock","paper","scissors"];
+    const randIdx=Math.floor(Math.random()*3);
+    return options[randIdx];
+ };
+
+ const drawGame=()=>{
+
+   msg.innerText="Game was draw.play again." 
+   msg.style.backgroundColor="#081b31";
+ };
+
+ const showWinner=(userWin,userChoice,computerChoice)=>{
+    if (userWin){
+        userScore++;
+        userScorePara.innerText=userScore;
+        msg.innerText=`you win! your ${userChoice} beats ${computerChoice}`;
+        msg.style.backgroundColor="green";
+    }else{
+        computerScore++;
+        computerScorePara.innerText=computerScore;
+       
+        msg.innerText=`you lose  ${computerChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor="red";
+    }
+ };
+
+
+ const playGame=(userChoice) =>{
+
+    const computerChoice=genComputerChoice();
+    
+
+    if(userChoice===computerChoice){
+        drawGame();
+    }else{
+        let userWin=true;
+        if(userChoice==="rock"){
+         userWin=computerChoice==="paper" ? false :true;
+        }else if(userChoice==="paper"){
+         userWin=computerChoice==="scissor"? false:true;   
+        }else{
+            userWin=computerChoice==="rock"? false:true;
+        }
+        showWinner(userWin,userWin,computerChoice);
+    }
+        
+ };
+ choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+        const userChoice=choice.getAttribute("Id");
+        playGame(userChoice);
+    });
+ });
+    
